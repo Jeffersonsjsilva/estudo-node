4504187cd27e0be5a7e6cc5b6a91a9cf078cea12
jeffersonsjsilva <jeffe.santana19@gmail.com>
2024-03-11 17:09:35 -0300

    aula-11-03

--- express/protocolo-http/index.js
+++ express/protocolo-http/index.js
@@ -1,31 +1,59 @@
+//biblioteca express
 var express = require('express');
 
 const app = express();
 
 const port = 8081;
 
-//rota chamando o html
-app.get("/", function(req,res){
-res.sendFile(__dirname +"/html/index.html");
+// //rota chamando o html
+// app.get("/", function(req,res){
+// res.sendFile(__dirname +"/html/index.html");
+// });
+
+// app.get("/sobre",function(req,res){
+//     res.sendFile(__dirname + "/html/sobre.html");
+// });
+
+// app.get("/produtos",function(req,res){
+//     res.send("Produtos da empresa");
+// });
+// //rota com parâmetros
+
+// app.get("/ola/:nome/:cargo/:idade", function(req,res){
+//     res.send('<h1> Ola ' + req.params.nome + 
+//     '<h1>'+ '<br> <h2> Seu cargo é: ' + req.params.cargo+ '<br> Sua idade é: ' + req.params.idade +
+//     '</h2>');
+// });
+
+
+//requirindo biblioteca handlebars
+const handlebars = require('express-handlebars');
+
+
+const sequelize = require("sequelize"); //requirindo modulo sequelize para conexão e administração do BD
+
+const sequelize2 = new sequelize('testenode', 'root', '', {
+    host: "localhost",
+    dialect: 'mysql'
 });
 
-app.get("/sobre",function(req,res){
-    res.sendFile(__dirname + "/html/sobre.html");
+//criar rota get
+app.get('/cad', function (req, res) {
+    res.render('formulario.handlebars');
 });
 
-app.get("/produtos",function(req,res){
-    res.send("Produtos da empresa");
+//criando rota POST
+app.post('/add', function (req, res) {
+    res.send('FORMULARIO RECEBIDO');
 });
-//rota com parâmetros
 
-app.get("/ola/:nome/:cargo/:idade", function(req,res){
-    res.send('<h1> Ola ' + req.params.nome + 
-    '<h1>'+ '<br> <h2> Seu cargo é: ' + req.params.cargo+ '<br> Sua idade é: ' + req.params.idade +
-    '</h2>');
-});
+//configurando o handlebars e o template engine
+
+app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
+app.set('view.engine', 'handlebars');
 
 //porta do protocolo http
-app.listen(port, function(){
-console.log('servidor funcionando no endereço http://localhost:8081:');
+app.listen(port, function () {
+    console.log('servidor funcionando no endereço http://localhost:8081:');
 }
 );
