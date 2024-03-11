9671a844ed70bd6c58978e33376920c6fbd33c26
jeffersonsjsilva <jeffe.santana19@gmail.com>
2024-03-11 15:56:55 -0300

    bancodedados-sequelize

--- express/protocolo-http/index.js
+++ express/protocolo-http/index.js
@@ -10,19 +10,19 @@ res.sendFile(__dirname +"/html/index.html");
 });
 
 app.get("/sobre",function(req,res){
-    res.sendFile(__dirname + "/html/sobre.html")
-})
+    res.sendFile(__dirname + "/html/sobre.html");
+});
 
 app.get("/produtos",function(req,res){
     res.send("Produtos da empresa");
-})
+});
 //rota com parâmetros
 
 app.get("/ola/:nome/:cargo/:idade", function(req,res){
     res.send('<h1> Ola ' + req.params.nome + 
     '<h1>'+ '<br> <h2> Seu cargo é: ' + req.params.cargo+ '<br> Sua idade é: ' + req.params.idade +
     '</h2>');
-})
+});
 
 //porta do protocolo http
 app.listen(port, function(){
---
+++ express/protocolo-http/testenode.js
@@ -0,0 +1,65 @@
+const sequelize = require("sequelize"); //requirindo modulo sequelize para conexão e administração do BD
+
+const sequelize2 = new sequelize('testenode', 'root', '', {
+    host: "localhost",
+    dialect: 'mysql'
+});//Criando conexão com o banco de dados
+
+
+//Criando tabela no banco de dados
+const postagem = sequelize2.define('postagens', {
+    titulo:{
+        type:sequelize.STRING(100)
+    },
+    conteudo:{
+        type: sequelize.TEXT
+    },
+
+});
+//criando outra tabela
+const usuario = sequelize2.define('usuario',{
+    nome: {
+        type: sequelize.STRING(50)
+    },
+    sobrenome:{
+        type: sequelize.STRING(50)
+    },
+    idade: {
+        type: sequelize.INTEGER
+    },
+    email: {
+        type: sequelize.STRING(100)
+    }
+
+})
+//Executar a linha abaixo apenas 1vez
+// usuario.sync({force:true});
+// postagem.sync({force:true});
+
+//inserindo dados nas tabelas
+
+postagem.create({
+    titulo:"Promoção dia das mães",
+    conteudo: "No mês do dia das mães todas as lojas terão 50% de desconto nos esmaltes"
+
+})
+
+usuario.create({
+    nome: "Jefferson",
+    sobrenome: "Santana",
+    idade: "26",
+    email: "jefferson.sjsilva@senacsp.edu.br"
+})
+
+
+
+
+
+// TESTE DE CONEXÃO COM O BANCO
+/* sequelize2.authenticate().then(function(){
+    console.log("Conexão efetudada com sucesso");
+}
+).catch(function(erro){
+    console.log(`Falha ao se conectar ${erro}`);
+ }); */
+
